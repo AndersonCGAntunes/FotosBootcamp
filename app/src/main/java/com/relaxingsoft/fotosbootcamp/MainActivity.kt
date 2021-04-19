8881a066de5c6c5cf074b94f5aa320b1b620064c
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-04-19 09:46:01 -0300

    Desenvolvimento inicial da MainActivity

--- app/src/main/java/com/relaxingsoft/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/relaxingsoft/fotosbootcamp/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private val PERMISSION_CODE_IMAGE_PICK = 1000
         private val IMAGE_PICK_CODE = 1001
+
+        private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +36,24 @@ class MainActivity : AppCompatActivity() {
                 pickImageFromGalery()
             }
         }
+
+        open_camera_button.setOnClickListener {
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+                if ( checkSelfPermission( Manifest.permission.CAMERA )
+                        == PackageManager.PERMISSION_DENIED ||
+                        checkSelfPermission( Manifest.permission.WRITE_EXTERNAL_STORAGE )
+                        == PackageManager.PERMISSION_DENIED ) {
+                    val permissions = arrayOf( Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE )
+                    requestPermissions( permissions, PERMISSION_CODE_CAMERA_CAPTURE )
+                } else {
+                    openCamera()
+                }
+            } else {
+                openCamera()
+            }
+        }
+
     }
 
     override fun onRequestPermissionsResult(
@@ -49,6 +69,17 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText( this, "Permissão Negada", Toast.LENGTH_SHORT ).show()
                 }
             }
+
+            PERMISSION_CODE_CAMERA_CAPTURE -> {
+                if ( grantResults.size > 1
+                        && grantResults[ 0 ] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[ 1 ] == PackageManager.PERMISSION_GRANTED ) {
+                    openCamera()
+                } else {
+                    Toast.makeText( this, "Permissão Negada", Toast.LENGTH_SHORT ).show()
+                }
+            }
+
         }
     }
 
